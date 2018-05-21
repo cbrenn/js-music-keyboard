f3c7245d8567135c0615be1a5bea16fd0dbd31f9
Cara <cararbrennan@gmail.com>
2018-05-21 16:30:36 -0700

    Keys triggered by mouseclicks

--- noise.js
+++ noise.js
@@ -1,3 +1,41 @@
+
+
+
+
 $(document).ready( function() {
+
+  $('.note.a').on ('click', () => {
+    var audio = document.getElementById("aAudio");
+    aAudio.play();
+  });
+
+  $('.note.b').on ('click', () => {
+    var audio = document.getElementById("bAudio");
+    bAudio.play();
+  });
+  $('.note.c').on ('click', () => {
+    var audio = document.getElementById("cAudio");
+    cAudio.play();
+  });
+  $('.note.d').on ('click', () => {
+    var audio = document.getElementById("dAudio");
+    dAudio.play();
+  });
+  $('.note.e').on ('click', () => {
+    var audio = document.getElementById("eAudio");
+    eAudio.play();
+  });
+
+  $('.note.f').on ('click', () => {
+    var audio = document.getElementById("fAudio");
+    fAudio.play();
+  });
+
+  $('.note.g').on ('click', () => {
+    var audio = document.getElementById("gAudio");
+    gAudio.play();
+  });
+
+  
   // your code here
 });
