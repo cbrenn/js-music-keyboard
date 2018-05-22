a76df6ebdf72da720f3a5bfe4f2fa8d929cd9272
Cara <cararbrennan@gmail.com>
2018-05-21 17:24:31 -0700

    plays notes on key up event

--- noise.js
+++ noise.js
@@ -1,41 +1,41 @@
-
-
-
-
 $(document).ready( function() {
+  $('body').on ('keydown', function(event) {
+    console.log(event)
+    if (event.keyCode==65){
+      const audio = document.getElementById("aAudio");
+      audio.play();
+      audio.currentTime = 0;
+    }else if (event.keyCode==66){
+      const audio = document.getElementById("bAudio");
+      audio.play();
+      audio.currentTime = 0;
+    }else if (event.keyCode==67){
+      const audio = document.getElementById("cAudio");
+      audio.play();
+      audio.currentTime = 0;
+    }else if (event.keyCode==68){
+      const audio = document.getElementById("dAudio");
+      audio.play();
+      audio.currentTime = 0;
+    }else if(event.keyCode==69){
+      const audio = document.getElementById("eAudio");
+      audio.play();
+      audio.currentTime = 0;
+    }else if(event.keyCode==70){
+      const audio = document.getElementById("fAudio");
+      audio.play();
+      audio.currentTime = 0;
+    }else if(event.keyCode==71){
+      const audio = document.getElementById("gAudio");
+      audio.play();
+      audio.currentTime = 0;
+    }else{
+      alert ("ABCDEFG only please!");
+    }dd
 
-  $('.note.a').on ('click', () => {
-    var audio = document.getElementById("aAudio");
-    aAudio.play();
   });
-
-  $('.note.b').on ('click', () => {
-    var audio = document.getElementById("bAudio");
-    bAudio.play();
-  });
-  $('.note.c').on ('click', () => {
-    var audio = document.getElementById("cAudio");
-    cAudio.play();
-  });
-  $('.note.d').on ('click', () => {
-    var audio = document.getElementById("dAudio");
-    dAudio.play();
-  });
-  $('.note.e').on ('click', () => {
-    var audio = document.getElementById("eAudio");
-    eAudio.play();
-  });
-
-  $('.note.f').on ('click', () => {
-    var audio = document.getElementById("fAudio");
-    fAudio.play();
-  });
-
-  $('.note.g').on ('click', () => {
-    var audio = document.getElementById("gAudio");
-    gAudio.play();
-  });
-
-  
-  // your code here
 });
+
+
+
+// your code here
